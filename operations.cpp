87f52547c604099dbf4bc0d94afdfa8d3ba70e4d
Pierson Bian <pierson.bian@gmail.com>
2018-06-08 13:39:06 -0700

    Add files via upload

--- operations.cpp
+++ operations.cpp
@@ -1,4 +1,5 @@
 #include "operations.h"
+#include <cmath>
 
 Operations::Operations(){
     //need to populate _locations based on _graph
@@ -11,4 +12,11 @@ node* Operations::find(std::string name){
 }
 
 double Operations::distance(node *start, node *end){
+    double radius = 3959, distance = 0;
+    distance = radius * (atan(sqrt(pow(cos(end->latitude) * sin(abs(start->longitude - end->longitude)), 2)
+                                   + pow(cos(start->latitude) * sin(end->latitude) - sin(start->latitude)
+                                         * cos(end->latitude) * cos(abs(start->longitude - end->longitude)), 2))
+                              / (sin(start->latitude) * sin(end->latitude) + cos(start->latitude) * cos(end->latitude)
+                                 * cos(abs(start->longitude - end->longitude)))));
+    return distance;
 }
